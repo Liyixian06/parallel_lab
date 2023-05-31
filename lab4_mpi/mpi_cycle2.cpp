02d980db7a59972814e4ab21c8b5303d81aeea01
Liyixian06 <2575597322@qq.com>
2023-05-31 22:14:27 +0800

    lab_4

---
+++ lab4_mpi/mpi_cycle2.cpp
@@ -0,0 +1,77 @@
+#include <sys/time.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+using namespace std;
+const int N=1000;
+float m[N][N];
+void m_reset()
+{
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<i;j++)
+			m[i][j]=0;
+		m[i][i]=1.0;
+		for(int j=i+1;j<N;j++)
+			m[i][j]=rand();
+	}
+	for(int k=0;k<N;k++)
+		for(int i=k+1;i<N;i++)
+			for(int j=0;j<N;j++)
+				m[i][j]+=m[k][j];
+}
+int main(){
+	struct timeval start;
+	struct timeval end;
+
+	int rank; //进程数
+	int size; //当前是第几个进程
+	MPI_Init(NULL,NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	//0号进程初始化矩阵，发送到所有进程
+	if (rank == 0) {
+		m_reset();
+		for (int i = 1; i < size; i++) {
+            		MPI_Send(&m[0][0], N*N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+		}
+	}
+	else MPI_Recv(&m[0][0], N*N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+	//循环划分
+	gettimeofday(&start,NULL);
+    	for (int k = 0; k < N; k++) {
+		if (k % size == rank) {
+            		for (int j = k + 1; j < N; j++) {
+                			m[k][j] /= m[k][k];
+            		}
+            		m[k][k] = 1.0;
+			//将除法结果一对多广播给其他进程
+            		for (int p = 0; p < size; p++) {
+                			if (p != rank) {
+                    			MPI_Send(&m[k][0], N, MPI_FLOAT, p, 1, MPI_COMM_WORLD);
+                			}
+            		}
+        		}
+		else {
+            		MPI_Recv(&m[k][0], N, MPI_FLOAT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); }
+		int l;
+		if( N/size*size + rank < N) l = N/size*size + rank;
+		else l = N/size*size + rank - size;
+		for (int i = l; i > k; i-=size) {
+            		for (int j = k+1; j < N; j++) {
+                			m[i][j] -= m[i][k] * m[k][j];
+            		}
+            		m[i][k] = 0;
+		}
+    	}
+	MPI_Finalize();
+
+	gettimeofday(&end,NULL);
+    	float time = (end.tv_sec-start.tv_sec)*1000000+end.tv_usec-start.tv_usec;
+	cout<<time<<endl;
+	return 0;
+}
+
---
+++ lab4_mpi/mpi_sse.cpp
@@ -0,0 +1,101 @@
+#include <sys/time.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <immintrin.h>
+using namespace std;
+const int N=1000;
+float m[N][N];
+void m_reset()
+{
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<i;j++)
+			m[i][j]=0;
+		m[i][i]=1.0;
+		for(int j=i+1;j<N;j++)
+			m[i][j]=rand();
+	}
+	for(int k=0;k<N;k++)
+		for(int i=k+1;i<N;i++)
+			for(int j=0;j<N;j++)
+				m[i][j]+=m[k][j];
+}
+int main(){
+	struct timeval start;
+	struct timeval end;
+
+	int rank; //进程数
+	int size; //当前是第几个进程
+	MPI_Init(NULL,NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	//设置进程分配范围[r1,r2]
+	int tasknum = N/size;
+	int r1 = rank * tasknum;
+	int r2;
+	if(rank!=size-1)
+		r2 = rank * tasknum + tasknum -1;
+	else r2 = N-1;
+
+	//0号进程初始化矩阵，发送到所有进程
+	if (rank == 0) {
+		m_reset();
+		for (int i = 1; i < size; i++) {
+            		MPI_Send(&m[0][0], N*N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+		}
+	}
+	else MPI_Recv(&m[0][0], N*N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+	//块划分
+	gettimeofday(&start,NULL);
+    	for (int k = 0; k < N; k++) {
+		if (k >= r1 && k <= r2) {
+            		__m128 vt=_mm_set1_ps(m[k][k]);
+			for(int j=k+1;j<N;j+=4){
+				if(j+4>N)
+					for(;j<N;j++)
+						m[k][j]=m[k][j]/m[k][k];
+				else{
+					__m128 va=_mm_loadu_ps(m[k]+j); //alignment not required
+					va=_mm_div_ps(va,vt);
+					_mm_storeu_ps(m[k]+j,va);
+				}
+			}
+            		m[k][k] = 1.0;
+			//将除法结果一对多广播给其他进程
+            		for (int p = 0; p < size; p++) {
+                			if (p != rank) {
+                    			MPI_Send(&m[k][0], N, MPI_FLOAT, p, 0, MPI_COMM_WORLD);
+                			}
+            		}
+        		}
+		else {
+            		MPI_Recv(&m[k][0], N, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); }
+		for (int i = max(r1, k+1); i <=r2; i++) {
+            		for(int j=k+1;j<N;j+=4){
+				if(j+4>N)
+					for(;j<N;j++)
+						m[i][j]=m[i][j]-m[i][k]*m[k][j];
+				else{
+					__m128 v1=_mm_loadu_ps(m[i]+j);
+					__m128 v2=_mm_loadu_ps(m[k]+j);
+					__m128 v3=_mm_set1_ps(m[i][k]);
+					v2=_mm_mul_ps(v2,v3);
+					v1=_mm_sub_ps(v1,v2);
+					_mm_storeu_ps(m[i]+j,v1);
+				}
+			}
+			m[i][k]=0;
+        		}
+    	}
+	MPI_Finalize();
+
+	gettimeofday(&end,NULL);
+    	float time = (end.tv_sec-start.tv_sec)*1000000+end.tv_usec-start.tv_usec;
+	cout<<time<<endl;
+	return 0;
+}
+
